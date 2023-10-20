fbe9037dc5c20e0cee53c3d73a7bf8137fdc2bda
priyanka verma <pv209304@gmail.com>
2023-10-20 15:15:30 +0530

    adding edited files

--- Routs/empviewrout.js
+++ Routs/empviewrout.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-const viewsController=require('../Controller/empviewcontroller');
+const viewsController=require('../Controller/empViewController');
 router.get('/view',viewsController.views);
 
 module.exports=router;
